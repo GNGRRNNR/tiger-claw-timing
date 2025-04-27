53c77c65296c0aef7d949070280990d0f53b7036
GNGRRNNR <THEGINGERRUNNER@GMAIL.COM>
2025-04-26 17:43:48 -0700

    Create service-worker.js

---
+++ ascent-finishline/service-worker.js
@@ -0,0 +1,52 @@
+// ascent-finishline/service-worker.js
+
+const CACHE_NAME = 'ascent-finishline-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[Service Worker] Caching app shell');
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => {
+        console.error('[Service Worker] Error caching', err);
+      })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activate');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName !== CACHE_NAME;
+        }).map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+// Fetch requests
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith('chrome-extension://')) {
+    return; // Ignore Chrome extension requests
+  }
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
